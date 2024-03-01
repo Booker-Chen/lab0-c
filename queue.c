54abf076470a8a7bacecf0f1501d14bdc2138167
Booker-Chen <booker0216@gmail.com>
2024-03-02 02:26:54 +0800

    Complete function q_new and q_free

--- queue.c
+++ queue.c
@@ -14,11 +14,28 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    if (!head) {
+        free(head);
+        return NULL;
+    }
+
+    INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+    while (!list_empty(head)) {
+        struct list_head *del = head->next;
+        list_del(del);
+        free(del);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
